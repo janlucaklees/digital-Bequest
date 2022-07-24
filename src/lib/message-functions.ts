f6457ad6593eb436cfca9761e5d27e07064fe42f
Jan-Luca Klees <email@janlucaklees.de>
2022-07-24 15:22:01 +0200

    Added types to the core fuctions

--- src/lib/message-functions.ts
+++ src/lib/message-functions.ts
@@ -1,11 +1,11 @@
 import CryptoJS from 'crypto-js';
 
 
-export function encodeMessage(message, password) {
+export function encodeMessage(message:string, password:string):string {
 	return CryptoJS.AES.encrypt(message, password).toString();
 }
 
-export function decodeMessage(cipher, password) {
+export function decodeMessage(cipher:string, password:string):string {
 	return CryptoJS.AES.decrypt(cipher, password).toString(CryptoJS.enc.Utf8);
 }
 
